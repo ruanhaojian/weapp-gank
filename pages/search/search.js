8bc1a22908eaa2b3e5cc6e1e92be52f1250b4209
ruanhaojian <610708075@qq.com>
2016-09-30 17:03:08 +0800

    load more

--- pages/search/search.js
+++ pages/search/search.js
@@ -3,12 +3,32 @@
 var app = getApp()
 var API = require('../../utils/api.js')
 
+Date.prototype.format = function (fmt) { //author: meizz
+        var Week = ['日', '一', '二', '三', '四', '五', '六'];
+        var o = {
+            "M+": this.getMonth() + 1, //月份 
+            "d+": this.getDate(), //日 
+            "h+": this.getHours(), //小时 
+            "m+": this.getMinutes(), //分 
+            "s+": this.getSeconds(), //秒 
+            "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+            "S": this.getMilliseconds(), //毫秒
+            "w": Week[this.getDay()]
+        };
+        if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        return fmt;
+    }
+
 Page({
   data: {
     isLoading: false,
     typeArray: ['all','Android','iOS','福利','App','拓展资源','前端','瞎推荐'],
-    index: 3,
-    items: []
+    index: 0,
+    items: [],
+    btnLoading: false,
+    page : 1
   },
   onLoad: function () {
     var _this = this;
@@ -21,23 +41,36 @@ Page({
     var _this = this;
 
     this.setData({
-      isLoading: true
+      btnLoading: true
     })
 
-    this.setData({
-      items: []
-    })
+    // this.setData({
+    //   items: []
+    // })
 
     API.getGankData({
       data:{
-        catogory: this.data.typeArray[this.data.index]
+        catogory: this.data.typeArray[this.data.index],
+        page : this.data.page
       },
       success: function(res){
 
-        res.data.results.map(function(item){
-  	      item.url = item.url.replace('//ww', '//ws');
-          return item;
-        })
+        if(_this.data.typeArray[_this.data.index] != '福利'){
+          res.data.results.map(function(item){
+            var date = new Date(item.publishedAt);
+            item.publishedAt = date.format('yyyy-MM-dd');
+            return item;
+          })
+        }else{
+          res.data.results.map(function(item){
+            item.url = item.url.replace('//ww', '//ws');
+            return item;
+          })
+        }
+        
+        if(_this.data.page > 1){
+          res.data.results = _this.data.items.concat(res.data.results);
+        }
 
         _this.setData({
           items: res.data.results
@@ -45,12 +78,12 @@ Page({
         console.dir(res.data.results)
 
         _this.setData({
-          isLoading: false
+          btnLoading: false
         })
       },
       error: function(e){
         _this.setData({
-          isLoading: false
+          btnLoading: false
         })
       }
     });
@@ -61,7 +94,15 @@ Page({
   },
   bindPickerChange: function(e){
     this.setData({
-      index: e.detail.value
+      index: e.detail.value,
+      items: [],
+      page: 1
+    })
+    this.searchData();
+  },
+  loadMore: function(){
+    this.setData({
+      page: this.data.page + 1
     })
     this.searchData();
   }
